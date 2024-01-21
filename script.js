08e0796d72d1efd4b593eb6ed8b0752581c153fa
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-01-21 14:07:23 +0000

    created timeblocks function to dynamically create <divs> in HTML

--- script.js
+++ script.js
@@ -12,4 +12,30 @@ $(document).ready(function () {
     var entry = $(this).siblings(".description").val(); 
     var time = $(this).parent().attr("id").toString(); 
     localStorage.setItem(time, entry);})
+
+
+//Each time block consists of a row with three columns: an hour column, a description column, and a save button column. 
+//The hour column displays the time in 12-hour format (AM/PM), and the description column is a textarea where users can enter notes.
+
+function createTimeBlock(hour) {
+  var timeofDay = hour >= 12 ? 'PM' : 'AM';
+  var displayHour = hour % 12 || 12;
+  return `
+      <div id="${hour}" class="row time-block">
+          <div class="col-md-1 hour">
+              ${displayHour}:00 ${timeofDay}
+          </div>
+          <textarea class="col-md-10 description"></textarea>
+          <button class="btn saveBtn col-lg-1"><i class="fas fa-save"></i></button>
+      </div>
+  `;
+}
+//The Array.from() method is used to create an array of hours from 8 to 17, 
+//and the .map() method is used to apply the createTimeBlock() function to each hour in the array. 
+//The resulting array of HTML strings is then joined into a single string and appended to the '.container' element on the page.
+
+var hours = Array.from({length: 10}, (_, i) => i + 8); 
+var timeBlocksHtml = hours.map(hour => createTimeBlock(hour)).join('');
+$('.container').append(timeBlocksHtml);
+
 });
