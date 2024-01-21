6bb16607391ccc10bf33b8002c1b1409c354fe5b
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-01-21 15:43:16 +0000

    added a function for past, present and future classes to show on the calendar

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 //Each time block consists of a row with three columns: an hour column, a description column, and a save button column. 
 //The hour column displays the time in 12-hour format (AM/PM), and the description column is a textarea where users can enter notes.
 
-function createTimeBlock(hour) {
+function createTimeblock(hour) {
   var timeofDay = hour >= 12 ? 'PM' : 'AM';
   var displayHour = hour % 12 || 12;
   return `
@@ -35,7 +35,34 @@ function createTimeBlock(hour) {
 //The resulting array of HTML strings is then joined into a single string and appended to the '.container' element on the page.
 
 var hours = Array.from({length: 10}, (_, i) => i + 8); 
-var timeBlocksHtml = hours.map(hour => createTimeBlock(hour)).join('');
+var timeBlocksHtml = hours.map(hour => createTimeblock(hour)).join('');
 $('.container').append(timeBlocksHtml);
 
+// for loop allows the code the efficient when retrieving values for each hour from local storage
+//i is initialized from hour 8 until 5pm
+for (var i = 8; i <= 17; i++) {
+  var hour = "#" + i;
+  $(hour + " .description").val(localStorage.getItem(i.toString()));
+}
+
+//this portion of the code gets the current system time using day.js and iterates over 
+//each time block element on the page. 
+//For each time block, it compares the block's hour with the current hour. 
+//Depending on the result, it adds or removes certain CSS classes to the time block:
+//if the block's hour is less than the current hour, the class "past" class is added and "future" and "present" classes are removed.
+//If the block's hour is equal to the current hour, the class "present" is added and  "past" and "future" classes are removed.
+//Otherwise, the "present" and "past" classes are removed and added the "future" class. 
+
+function calendarInteractivity() {
+  var currentTime = dayjs().hour(); 
+
+  $(".time-block").each(function () {
+      let $this = $(this);
+      var blockHour = parseInt($this.attr("id"));
+      blockHour < currentTime ? $this.addClass("past").removeClass("future").removeClass("present") :
+      blockHour === currentTime ? $this.removeClass("past").addClass("present").removeClass("future") :
+      $this.removeClass("present").removeClass("past").addClass("future");
+  })
+}
+calendarInteractivity(); 
 });
